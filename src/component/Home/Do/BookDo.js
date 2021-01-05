6f06ce6dfcb53f41b03d5ebe64f2bdcefade1b4a
SeonghoJin <seongho4996@naver.com>
2021-01-05 16:39:37 +0900

    bookDo 추가

---
+++ src/component/Home/Do/BookDo.js
@@ -0,0 +1,10 @@
+class BookDo{
+    constructor(){
+        this.image="";
+        this.title="";
+        this.author="";
+        this.description="";
+        this.pubdate="";
+        this.id="";
+    }
+}
